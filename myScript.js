31d81712f0b96d8b1cd5805059efab5e86bf8740
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-04-10 20:36:57 +0200

    modificato col nan

--- myScript.js
+++ myScript.js
@@ -1,7 +1,10 @@
-var km = prompt('Quanti km vuoi percorrere?');
-        var anni = prompt('Quanti anni hai?');
+var km = parseInt(prompt('Quanti km vuoi percorrere?'));
+        var anni = parseInt(prompt('Quanti anni hai?'));
         var priceatkm = 0.21;
         var price = priceatkm * km;
+        if(Number.isNaN(anni) || Number.isNaN(priceatkm)) {
+            price = "Non hai inserito correttamente un numero"
+        }
         if(anni < 18) {
             price = (price/100)*80;
             var price = price.toFixed(2);
